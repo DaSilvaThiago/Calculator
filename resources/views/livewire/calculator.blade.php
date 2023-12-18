0054fcd72e89ad34bbd14491f3240d887cbc6e2e
Thiago Dias da Silva <silvathiago011@hotmail.com>
2023-12-18 12:35:18 -0300

    calculator component is done

---
+++ resources/views/livewire/calculator.blade.php
@@ -0,0 +1,45 @@
+<div class="p-4">
+    <div class="container m-auto w-56 bg-gray-800 border border-white border-2 rounded-lg">
+        <div class="text-white text-center p-3">
+            <p>Thiago from Laravel | <span class="text-orange-300">Calculator</span></p>
+        </div>
+        <div class="bg-gray-600 flex flex-row-reverse">
+            <div class="flex flex-col p-3 mt-2">
+                <span class="text-white">200 + 200</span>
+                <span class="text-orange-300 flex justify-end">400</span>
+            </div>
+        </div>
+        <div class="container bg-gray-800 p-4">
+            <div class="flex justify-center">
+                <div class="flex flex-col">
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">%</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">7</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">4</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">1</button>
+                    <button class="bg-orange-300 text-white p-3 m-1 rounded-lg">C</button>
+                </div>
+                <div class="flex flex-col">
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">(</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">8</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">5</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">2</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">0</button>
+                </div>
+                <div class="flex flex-col">
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">)</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">9</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">6</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">3</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">.</button>
+                </div>
+                <div class="flex flex-col">
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">/</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">x</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">-</button>
+                    <button class="bg-gray-700 text-white p-3 m-1 rounded-lg">+</button>
+                    <button class="bg-orange-300 text-white p-3 m-1 rounded-lg">=</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+       <h1 class="text-9xl">{{ $count }}</h1>
+ 
+    <button wire:click="increment" >+</button>
+ 
+    <button wire:click="decrement">-</button>
+</div>
